0f2fd5ec86449ab140deaccf2ce8bcd473550a30
Nomer Orpiano <leyvey009@gmail.com>
2025-08-18 20:53:02 +0800

    Add files via upload
    Activity #1 Basic Math with Condition

---
+++ Mainc.java
@@ -0,0 +1,24 @@
+package main;
+
+class Mainc {
+
+	public static void main(String[] args) {
+
+		    // basic if else arithmetic 
+		    
+		    int a = 8;
+	        int b = 4;
+	        int result = 0;
+	        char op = '+';
+
+	        if (op == '+') {
+	            result = a + b;
+	            System.out.println("Addition: " + result);
+	        } else if (op == '-') {
+	            result = a - b;
+	            System.out.println("Subtraction: " + result);
+	}
+
+}
+
+}
