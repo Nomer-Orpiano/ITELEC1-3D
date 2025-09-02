1cfc43862b2d62a6386fa35f8495aa350563c12e
Nomer Orpiano <leyvey009@gmail.com>
2025-09-02 09:31:21 +0800

    Activity Life Cycle

--- MainActivity.kt
+++ MainActivity.kt
@@ -12,11 +12,52 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-private val MAIN_ACTIVITY_TAG = "Activity 1"
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        println("onCreate()")
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart()")
+    }
+    override fun onResume() {
+        super.onResume()
+        println("onResume()")
+    }
+    override fun onPause() {
+        super.onPause()
+        println("onPause()")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        println("onRestart()")
+    }
+    override fun onStop() {
+        super.onStop()
+        println("onStop()")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy()")
+    }
+
+
+
 }
