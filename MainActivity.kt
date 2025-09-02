9f565420de4174fb9adf40f40edae271ff44dd5d
Nomer Orpiano <leyvey009@gmail.com>
2025-09-02 09:27:25 +0800

    ActivityLifeCycle

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.greetingcard
+package com.example.myapplication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,41 +10,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.greetingcard.ui.theme.GreetingCardTheme
+import com.example.myapplication.ui.theme.MyApplicationTheme
 
+private val MAIN_ACTIVITY_TAG = "Activity 1"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            GreetingCardTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Nomer",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color) {
-        Text(
-            text = "Hi, my name is $name!",
-            modifier = modifier
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GreetingCardTheme {
-        Greeting("Nomer")
     }
 }
